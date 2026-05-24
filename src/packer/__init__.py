6cfb196f88693b834eeb7ebca0bfde82f00daf82
EMILIO <93140582+EMILIO888real@users.noreply.github.com>
2026-05-24 19:43:51 +0300

    Added `normalize_settings_keys` function to `utils`, it changes whitespace " " to "_", to be able to be usable for python variables.

--- src/packer/__init__.py
+++ src/packer/__init__.py
@@ -1,6 +1,6 @@
-from packer.utils import load_config, simple_merge_settings
+from packer.utils import load_config, simple_merge_settings, normalize_settings_keys
 from packer.main import Packer
 from packer.setup import main as setup
 from packer.change import main as commit_change
 
-__all__ = ['load_config', 'Packer', 'setup', 'commit_change', 'simple_merge_settings']
+__all__ = ['load_config', 'Packer', 'setup', 'commit_change', 'simple_merge_settings', 'normalize_settings_keys']
