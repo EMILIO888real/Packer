945d39d0e464fa8a2d2004467b1f3302a420f663
EMILIO <93140582+EMILIO888real@users.noreply.github.com>
2026-05-24 19:28:16 +0300

    Added `simple_merge_settings` function to utils for easier standart settings merge.

--- src/packer/__init__.py
+++ src/packer/__init__.py
@@ -1,6 +1,6 @@
-from packer.utils import load_config
+from packer.utils import load_config, simple_merge_settings
 from packer.main import Packer
 from packer.setup import main as setup
 from packer.change import main as commit_change
 
-__all__ = ['load_config', 'Packer', 'setup', 'commit_change']
+__all__ = ['load_config', 'Packer', 'setup', 'commit_change', 'simple_merge_settings']
